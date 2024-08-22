a2955f55b76a790d22a606bd14dd21119432d2aa
Bradley Hower <bradleyhower@proton.me>
2024-08-22 16:10:18 -0700

    file organization changes

--- test.py
+++ test.py
@@ -1,38 +1,12 @@
-import random
+import os
 
-# Keyboard mutator. Two random key swaps.
-def mutate(bases, mutation_rounds):
+def generation_top_one():
+  # Define the full file path
+  file_path = os.path.join('data', 'layout_scores.csv')
 
-  # Mutations
-  def mutations(mutation_bases):
-    children_mut_round = []
-    bases_length = int(len(mutation_bases[0]))
-    
-    for i in range(int(len(mutation_bases))):
-      point1 = random.randint(0, (bases_length - 1))
-      point2 = random.randint(0, (bases_length - 1))
-      base_list = list(mutation_bases[i])
-      swap1 = base_list[point1]
-      swap2 = base_list[point2]
-      base_list[point1] = swap2
-      base_list[point2] = swap1
-      base_back_to_string = "".join(base_list)
-      children_mut_round.append(base_back_to_string)
-    
-    return children_mut_round
+  # Write layout to file
+  with open(file_path, 'w', encoding='utf-8') as f:
+    f.write(f"asdf,sdfg\n")
 
-  children_mut_x = []
 
-  def mutation_turner(mutation_rnds):
-    nonlocal children_mut_x
-
-    for _ in range(mutation_rnds):
-      children_mut_x = mutations(bases)
-      print(children_mut_x)
-  
-  mutation_turner(mutation_rounds)
-  print(children_mut_x)
-
-  return children_mut_x
-
-mutate(["7532", "1894"], 1)
+generation_top_one()
